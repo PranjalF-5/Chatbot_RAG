e166780467f8314a034d12d8abe175e47cf1f600
Pranjal Mayank <pranjalmayank555@gmail.com>
2026-05-24 16:40:34 +0530

    folder structure created

---
+++ template.sh
@@ -0,0 +1,17 @@
+# creating the folders
+
+mkdir -p src
+mkdir -p reseacrh
+
+# creatig files
+
+touch src/__main__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch research/trials.ipynb
+touch requirements.txt
+
+echo "Project structure created successfully!"
